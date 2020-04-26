48105d2a928affeea5053f60c87a96bc2cee9a67
AlbusSong <strawberry1234561@gmail.com>
2020-04-26 14:59:40 +0300

    finish UI of authorize

---
+++ pages/mine/authorize/authorize.js
@@ -0,0 +1,75 @@
+// pages/mine/authorize/authorize.js
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+
+  },
+
+  authorizeButtonClicked: function() {
+    console.log("authorizeButtonClicked");
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      }
+    })
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  }
+})
