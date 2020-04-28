2c65d6f109233a6cc432969a85ad440003975ade
AlbusSong <strawberry1234561@gmail.com>
2020-04-28 17:58:10 +0300

    selectSpecialitiesView

--- pages/post/create/create.js
+++ pages/post/create/create.js
@@ -10,12 +10,9 @@ Page({
   data: {
     shouldShowChosenSpecialities: false,
     expiringDate: "2020-04-27",
-  },
-
-  tryToChooseSpeciality: function(event) {
-    this.setData({
-      shouldShowChosenSpecialities: true
-    });
+    shouldShowSpecialitiesSelectionView: false,
+    arrOfSelectedIndexForSpecialities: [],
+    arrOfSelectedIndexForSpecialitiesForReal: []
   },
 
   datePickerChanged: function(e) {
@@ -23,6 +20,52 @@ Page({
     this.setData({
       expiringDate: e.detail.value,
     });
+  },
+
+  tryToChooseSpeciality: function(event) {
+    this.setData({
+      shouldShowChosenSpecialities: true
+    });
+    this.setData({
+      shouldShowSpecialitiesSelectionView: true
+    });
+  },
+
+  selectSpecialitiesViewBackgroundClicked: function(event) {
+    console.log(event);
+    this.arrOfSelectedIndexForSpecialities = [];
+    this.setData({
+      shouldShowSpecialitiesSelectionView: false
+    });
+  },
+
+  selectSpecialitiesViewBtnCancelClicked: function(event) {
+    this.arrOfSelectedIndexForSpecialities = [];
+    this.setData({
+      shouldShowSpecialitiesSelectionView: false
+    });
+  },
+
+  selectSpecialitiesViewBtnConfirmClicked: function(event) {
+    this.arrOfSelectedIndexForSpecialities = [];
+    this.setData({
+      shouldShowSpecialitiesSelectionView: false,
+      arrOfSelectedIndexForSpecialitiesForReal: this.data.arrOfSelectedIndexForSpecialities
+    });    
+  },
+
+  selectSpecialitiesItemCellClicked: function(event) {
+    let itemIndex = event.currentTarget.dataset.index;
+    let indexInArr = this.data.arrOfSelectedIndexForSpecialities.indexOf(itemIndex);
+    if (indexInArr == -1) {
+      this.data.arrOfSelectedIndexForSpecialities.push(itemIndex);
+    } else {
+      this.data.arrOfSelectedIndexForSpecialities.splice(indexInArr, 1);
+    }
+    this.setData({
+      arrOfSelectedIndexForSpecialities: this.data.arrOfSelectedIndexForSpecialities,
+    });
+    console.log(this.data.arrOfSelectedIndexForSpecialities);
   },
 
   /**
