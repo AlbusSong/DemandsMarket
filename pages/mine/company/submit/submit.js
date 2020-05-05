580fdf4d9d7c5fd56a3d3aa0f966e1f4e3a4c157
AlbusSong <strawberry1234561@gmail.com>
2020-05-05 14:13:41 +0300

    choose_speciality

--- pages/mine/company/submit/submit.js
+++ pages/mine/company/submit/submit.js
@@ -7,12 +7,56 @@ Page({
    * Page initial data
    */
   data: {
+    shouldShowChooseSpecialityView: false,
+
     companyName: "",
     companyOwnerName: "",
     companyRegisterNo: "",
     companyNo: "",
-    registerDate: "",
-    expiringDate: "",
+    registerDate: "请选择 >",
+    expiringDate: "请选择 >",
+    provinceAndCity: "请选择 >",
+    companySpecialityList: null,
+    selectedCompanySpeciality: "请选择 >",
+    selectedIndexForCompanySpeciality: -1,
+  },
+
+  cityPickerChanged: function(e) {
+    console.log(e);
+    this.data.provinceAndCity = e.detail.value.join("-");
+    this.setData({
+      provinceAndCity: this.data.provinceAndCity,
+    });
+  },
+
+  choose_speciality_wrapper_clicked: function() {
+    console.log("choose_speciality_wrapper_clicked");
+    this.data.shouldShowChooseSpecialityView = false;
+    this.setData({
+      shouldShowChooseSpecialityView: this.data.shouldShowChooseSpecialityView,
+    });
+  },
+
+  tryToChooseCompanySpeciality: function() {
+    this.data.shouldShowChooseSpecialityView = true;
+    this.setData({
+      shouldShowChooseSpecialityView: true,
+    });
+  },
+
+  companySpecialityItemClicked: function(e) {
+    let itemIndex = e.currentTarget.dataset.index;
+    this.data.selectedIndexForCompanySpeciality = itemIndex;
+    let selectedCompanySpeciality = this.data.companySpecialityList[itemIndex].speciality;
+    this.data.selectedCompanySpeciality = selectedCompanySpeciality;
+    this.setData({
+      selectedIndexForCompanySpeciality: itemIndex,
+      selectedCompanySpeciality: this.data.selectedCompanySpeciality,
+    });
+
+    setTimeout(() => {
+      this.choose_speciality_wrapper_clicked();      
+    }, 200);    
   },
 
   ocrButtonClicked: function(e) {
@@ -41,12 +85,23 @@ Page({
    * Lifecycle function--Called when page load
    */
   onLoad: function (options) {
-    this.data.registerDate = global_tool.getTodayDate();
-    this.data.expiringDate = global_tool.getTodayDate();
-    this.setData({
-      registerDate: this.data.registerDate,
-      expiringDate: this.data.expiringDate,
+    // 获取公司行业列表
+    var that = this;
+    getApp().func.postServer("company/get_company_speciality_list", {}, function(r) {      
+      console.log("company/get_company_speciality_list:", r);
+      that.data.companySpecialityList = r.data;
+      console.log("fsfasldfsf:", (that.data.companySpecialityList instanceof Array));
+      that.setData({
+        companySpecialityList: r.data,
+      });
     });
+
+    // this.data.registerDate = global_tool.getTodayDate();
+    // this.data.expiringDate = global_tool.getTodayDate();
+    // this.setData({
+    //   registerDate: this.data.registerDate,
+    //   expiringDate: this.data.expiringDate,
+    // });
   },
 
   /**
